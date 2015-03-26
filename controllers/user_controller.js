b1eb89815d58f84b70b0d97dde6c70fada794924
asynte <hung_yu@hotmail.com>
2015-03-27 00:11:55 +0800

    Start user controller
    Finished login, change password, starting has set super user

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,29 +3,62 @@ var User = mongoose.model('User');
 
 exports.login = function (req, res) {
     if (typeof req.body.email !== 'string') {
-        res.json({ code: 1,
-                   message: "邮箱为空"});
+        res.json({ code: 1, // Error code 1 means string error
+                    message: "邮箱为空"});
     }
 
     if (typeof req.body.password !== 'string') {
-        // error handler
+        res.json({ code: 1, // Error code 1 means string error
+                    message: "密码为空" });
     }
-    
+
     User.findOne({ email: req.body.email }, function (err, user) {
         if (err) {
-            //
+            res.json({ code: 100,
+                        message: "undefined"});
         } 
 
         if (user) {
             if (user.authenticate(req.body.password)) {
-                res.json({code: 0})
+                res.json({code: 0,
+                            message: "Authentication Correct"});
             } else {
-                res. 
+                res.json({ code: 2, //Error code 2 means email or password error
+                            message: "Email or Password error"});
             } 
         }
         else {
             //no such email in my database 
+            res.json({ code: 2,
+                        message: "Email or Password error"});
         }
-    })
+    });
+}
 
+exports.changePassword = function (req, res){
+    User.findById({ id: req.body.id}, function (err, user){
+        if(err){
+            res.json({ code: 100,
+                        message: "undefined"});
+        }
+        if(user){
+            user.password = req.body.password; // changing password
+            if(typeof user.password !== 'string'){ // Change password error
+                res.json({ code: 1,
+                            message: "密码为空"});
+            }
+            else{ //Change password success
+                user.save();
+                res.json({ code:0
+                            message: "Change Password Complete"});
+            }
+        }
+    });
+}
+
+exports.hasSetSuperUser = function (req, res){
+    User.findById( { id: req.body.id}, function(err, user){
+        //Using userid, we determin if this acc, has superpowers
+
+    })
 }
