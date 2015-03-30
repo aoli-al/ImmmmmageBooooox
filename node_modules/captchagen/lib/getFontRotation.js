5ca34b15595afb940e19120ea78eb4ec4e94f42d
Leeleo3x <leeleo3x@gmail.com>
2015-03-30 09:10:30 +0800

    remove some dependency

--- node_modules/captchagen/lib/getFontRotation.js
+++
@@ -1,3 +0,0 @@
-module.exports = function() {
-  return (Math.random() * -0.4) + 0.2;
-};
--- node_modules/captchagen/lib/randomBetween.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (from, to) {
-  return Math.floor(Math.random()*(to-from))+from;
-};
